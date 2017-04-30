fee1e739341551fad8de7fb6072d8984243543ff
Han Khuu <h.khuu01@gmail.com>
2017-04-30 19:54:09 -0400

    added image and updated files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Angry Bird Burger', 0, 'YYYY-MM-DD HH:MM:SS');
-
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Madd Cow Burger', 0, 'YYYY-MM-DD HH:MM:SS');
-
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Pleasant Plant Burger', 0, 'YYYY-MM-DD HH:MM:SS');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`, `date`) VALUES ('1', 'Angry Bird Burger', 0, '2017-04-30');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`, `date`) VALUES ('2', 'Madd Cow Burger', 0, '2017-04-30');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`, `date`) VALUES ('3', 'Pleasant Plant Burger', 0, '2017-04-30');
